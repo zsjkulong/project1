62914a5729fe480308c5f3ae9ae6b861c844b0bf
TSKY.sijian.zhou <zhousijian@travelsky.com>
2019-03-25 17:25:02 +0800

    ss

--- src/main/java/com/zsj/mybatis/service/BatchImportServiceImpl.java
+++ src/main/java/com/zsj/mybatis/service/BatchImportServiceImpl.java
@@ -28,10 +28,10 @@ public class BatchImportServiceImpl {
 
 	@Autowired
 	TestRest testRest;
-	
+
 	@Value("${key}")
 	String key;
-	
+
 	String[] keys = { "name", "sex", "birth", "mobile", "cityName", "mediaSource", "productCode", "policyBeginTime",
 			"supplierName", "supplierType", "disTestOrfree" };
 
@@ -50,9 +50,38 @@ public class BatchImportServiceImpl {
 			"Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0",
 			"Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0)",
 			"Mozilla/5.0 (iPhone; CPU iPhone OS 5_1_1 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9B206 Safari/7534.48.3" };
-	
-	String asdf[] = {"200101","200102","200103","200104","200105","200106","200107","200108"};
-	
+
+	String income[] = { "200101", "200102", "200103", "200104", "200105", "200106", "200107", "200108" };
+
+	String caServingMedia[] = { "300101", "300102", "300103", "300103", "300104", "300105", "300201", "300202",
+			"300203", "300204", "300205", "300206", "300207", "300208", "300209", "300210", "300211", "300212",
+			"300213", "300214", "300215", "300216", "300217", "300218", "300219", "300301", "300302", "300303",
+			"300304", "300305", "300306", "300307", "300308", "300309", "300310", "300311", "300312", "300313",
+			"300314", "300315", "300316", "300317", "300318", "300319", "300320", "300321", "300322", "300323",
+			"300324", "300325", "300401", "300402", "300403", "300404", "300405", "300406", "300407", "300408",
+			"300409", "300410", "300411", "300412", "300413", "300414", "300415", "300416", "300417", "300418",
+			"300419", "300420", "300421", "300422", "300423", "300424", "300425", "300426", "300427", "300428",
+			"300429", "300430", "300431", "300432", "300501", "300502", "300503", "300504", "300505", "300601",
+			"300602", "300603", "300604", "300605", "300606", "300607", "300608", "300609", "300610", "300611",
+			"300612", "300613", "300614", "300615", "300616", "300617", "300618", "300619", "300620", "300621",
+			"300622", "300623", "300624", "300625", "300626", "300627", "300701", "300702", "300703", "300704",
+			"300705", "300706", "300707", "300708", "300709", "300710", "300711", "300712", "300801", "300802",
+			"300803", "300804", "300805", "300806", "300807", "300808", "300809", "300810", "300811", "300812",
+			"300901", "300902", "300903", "300904", "300905", "300906", "300907", "300908", "300909", "300910",
+			"300911", "301001", "301002", "301003", "301004", "301005", "301006", "301007", "301008", "301009",
+			"301010", "301101", "301102", "301103", "301104", "301105", "301106", "301107", "301201", "301202",
+			"301203", "301301", "301302", "301303", "301304", "301305", "301306", "301307", "301401", "301402",
+			"301403", "301404", "301405", "301406", "301501", "301502", "301503", "301504", "301505", "301506",
+			"301507", "301508", "301509" };
+
+	String caProductName[] = { "400101", "400102", "400103", "400104", "400201", "400202", "400203", "400204", "400205",
+			"400206" };
+
+	String caAsset[] = { "500101", "500102", "500103", "500104", "500105", "500301", "500302", "500303", "500304",
+			"500305", "500306" };
+
+	String caOccupation[] = { "600101", "600102", "600103", "600104", "600105", "600106", "600107", "600108" };
+
 	public String batchImport(File uploadfile) {
 		// File uploadfile = new File(fileName);
 		// 创建一个目录 （它的路径名由当前 File 对象指定，包括任一必须的父路径。）
@@ -100,9 +129,9 @@ public class BatchImportServiceImpl {
 	 * @return
 	 */
 	private void readExcelValue(Workbook workbook, String filePathName) {
-		
+
 		this.keys = this.key.split(",");
-		
+
 		// 获取所有的工作表的的数量
 		int numOfSheet = workbook.getNumberOfSheets();
 
@@ -127,12 +156,13 @@ public class BatchImportServiceImpl {
 			for (int j = 1; j <= lastRowNum; j++) {
 
 				if (j % 10 == 0) {
-					this.rateMap.put(filePathName.substring(filePathName.lastIndexOf("\\"), filePathName.length()).replace("\\", ""),
-							String.valueOf((((float)j / (float)lastRowNum) * 100)));
+					this.rateMap.put(filePathName.substring(filePathName.lastIndexOf("\\"), filePathName.length())
+							.replace("\\", ""), String.valueOf((((float) j / (float) lastRowNum) * 100)));
 				}
-				
-				if(j==lastRowNum){
-					this.rateMap.put(filePathName.substring(filePathName.lastIndexOf("\\"), filePathName.length()).replace("\\", ""),"100");
+
+				if (j == lastRowNum) {
+					this.rateMap.put(filePathName.substring(filePathName.lastIndexOf("\\"), filePathName.length())
+							.replace("\\", ""), "100");
 				}
 
 				row = sheet.getRow(j);
@@ -178,16 +208,16 @@ public class BatchImportServiceImpl {
 
 	}
 
-	private void putcaGuestTime(Map map, Row row){
+	private void putcaGuestTime(Map map, Row row) {
 		map.put("caGuestTime", System.currentTimeMillis());
-		map.put("caIncome","200106");
-		map.put("caServingMedia","300202");
-		map.put("caProductName","400202");
-		map.put("caAsset","500103");
-		map.put("caOccupation","600104");
-		
+		map.put("caIncome", "200106");
+		map.put("caServingMedia", "300202");
+		map.put("caProductName", "400202");
+		map.put("caAsset", "500103");
+		map.put("caOccupation", "600104");
+
 	}
-	
+
 	private void getValueFromExcelEachRow(Row row, int index, String key, Map map) {
 		row.getCell(index).setCellType(Cell.CELL_TYPE_STRING);
 		String value = row.getCell(index).getStringCellValue();
@@ -263,10 +293,10 @@ public class BatchImportServiceImpl {
 	}
 
 	public String getFileRate(String fileName) {
-		if(rateMap.get(fileName)==null){
+		if (rateMap.get(fileName) == null) {
 			return "rate:over";
 		}
-		
+
 		if (rateMap.get(fileName).equals("100")) {
 			this.rateMap.remove(fileName);
 			return "rate:over";
